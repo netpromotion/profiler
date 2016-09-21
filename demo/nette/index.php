8a491137744701516ef9d86ee373dd6cfb4af710
Petr Knap <dev@petrknap.cz>
2016-09-21 10:11:39 +0200

    Rearranged Nette demo

--- demo/nette/index.php
+++ demo/nette/index.php
@@ -8,29 +8,22 @@ require __DIR__ . "/../../vendor/autoload.php";
 
 ProfilerNetteExtension::enable(); // this is required only if you need to profile before container is created
 
-Profiler::start();
-{
-    $configurator = new Configurator();
+Profiler::start("Demo application");
 
-    Profiler::start();
-    {
-        $configurator->setDebugMode(TRUE);
-        $configurator->enableDebugger(__DIR__ . "/log");
-        $configurator->setTempDirectory(__DIR__ . "/temp");
-    }
-    Profiler::finish();
+Profiler::start("Configure application");
+$configurator = new Configurator();
+$configurator->setDebugMode(TRUE);
+$configurator->enableDebugger(__DIR__ . "/log");
+$configurator->setTempDirectory(__DIR__ . "/temp");
+$configurator->addConfig(__DIR__ . "/config/config.neon");
+Profiler::finish("Configure application");
 
-    Profiler::start();
-    {
-        $configurator->addConfig(__DIR__ . "/config/config.neon");
-        $container = $configurator->createContainer();
-    }
-    Profiler::finish();
+Profiler::start("Create container");
+$container = $configurator->createContainer();
+Profiler::finish("Create container");
 
-    Profiler::start();
-    {
-        $container->getService("application")->run();
-    }
-    Profiler::finish();
-}
-Profiler::finish();
+Profiler::start("Run");
+$container->getService("application")->run();
+Profiler::finish("Run");
+
+Profiler::finish("Demo application");
