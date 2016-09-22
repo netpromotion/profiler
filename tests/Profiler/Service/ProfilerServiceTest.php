2f1ea4447fc3ef1c83efd4c03068c6983aa7e125
Petr Knap <dev@petrknap.cz>
2016-09-22 09:47:13 +0200

    Added tests for ProfilerService

--- tests/Profiler/Service/ProfilerServiceTest.php
+++ tests/Profiler/Service/ProfilerServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Netpromotion\Profiler\Test\Service;
 
+use Netpromotion\Profiler\Profiler;
 use /** @noinspection PhpInternalEntityUsedInspection */ Netpromotion\Profiler\Service\ProfilerService;
 use PetrKnap\Php\Profiler\Profile;
 
@@ -61,9 +62,45 @@ class TracyBarAdapterTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @runInSeparateProcess
+     * @dataProvider dataIterateProfilesComputesRightTimeLineValues
+     * @param int[][] $times
+     * @param int[][] $percentages
      */
-    public function testIterateProfilesComputesRightTimeLineValues()
+    public function testIterateProfilesComputesRightTimeLineValues(array $times, array $percentages)
     {
-        $this->markTestSkipped();
+        /** @noinspection PhpInternalEntityUsedInspection */
+        $service = ProfilerService::getInstance();
+
+        foreach ($times as $time) {
+            $profile = new Profile();
+            $profile->meta[Profiler::START_TIME] = $time[0];
+            $profile->meta[Profiler::FINISH_TIME] = $time[1];
+            $profile->duration = $time[2];
+            $profile->absoluteDuration = $time[1] - $time[0];
+
+            /** @noinspection PhpInternalEntityUsedInspection */
+            $service->addProfile($profile);
+        }
+
+        $i = 0;
+        $service->iterateProfiles(function(Profile $profile) use ($percentages, &$i) {
+            /** @noinspection PhpInternalEntityUsedInspection */
+            $this->assertArraySubset([
+                ProfilerService::TIME_LINE_BEFORE => $percentages[$i][0],
+                ProfilerService::TIME_LINE_ACTIVE => $percentages[$i][1],
+                ProfilerService::TIME_LINE_INACTIVE => $percentages[$i][2],
+                ProfilerService::TIME_LINE_AFTER => $percentages[$i][3]
+            ], $profile->meta);
+            $i++;
+        });
+        $this->assertEquals(count($times), $i);
+    }
+
+    public function dataIterateProfilesComputesRightTimeLineValues()
+    {
+        return [
+            [[[0, 10, 5]], [[0, 50, 50, 0]]],
+            [[[0, 10, 5], [10, 20, 5]], [[0, 25, 25, 50], [50, 25, 25, 0]]]
+        ];
     }
 }
