bca604514a323cf24dc60f2a64932829d3a8e5b5
Petr Knap <dev@petrknap.cz>
2016-09-26 09:23:30 +0200

    maximebf/debugbar added to Lumen

--- demo/lumen/index.php
+++ demo/lumen/index.php
@@ -1,5 +1,6 @@
 <?php
 
+use DebugBar\DebugBar;
 use Laravel\Lumen\Application;
 use Netpromotion\Profiler\Profiler;
 
@@ -11,12 +12,29 @@ if (DEBUG_MODE === true) {
 
 Profiler::start(/* keep default label for better preview */);
 
+$headAppendix = "";
+$bodyAppendix = "";
+if (DEBUG_MODE === true) {
+    $debugBar = new DebugBar();
+    $debugBarRenderer = $debugBar->getJavascriptRenderer();
+    $headAppendix .= $debugBarRenderer->renderHead();
+    $bodyAppendix .= $debugBarRenderer->render();
+}
+
 $app = new Application(__DIR__);
 
 $app->get("/lumen/", function() use ($app) {
     return $app->welcome();
 });
 
+ob_start();
 $app->run();
+$output = ob_get_contents();
+ob_end_clean();
+
+$output = preg_replace('/<\/head>/i', $headAppendix . "</head>", $output, 1);
+$output = preg_replace('/<\/body>/i', $bodyAppendix . "</body>", $output, 1);
 
 Profiler::finish(/* keep default label for better preview */);
+
+print($output);
--- dockerfile
+++ dockerfile
@@ -1,3 +1,4 @@
 FROM php:5.4-apache
+RUN docker-php-ext-install mbstring
 COPY . /var/www/
 COPY ./demo /var/www/html/
