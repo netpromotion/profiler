4a4b21920601c3a3ff512f7b1d1ca72708b352a2
Petr Knap <dev@petrknap.cz>
2016-09-26 14:50:38 +0200

    Revert "maximebf/debugbar added to Lumen"
    This reverts commit bca604514a323cf24dc60f2a64932829d3a8e5b5.

--- demo/require_me.php
+++ demo/require_me.php
@@ -3,5 +3,3 @@
 define("DEBUG_MODE", true);
 
 require_once __DIR__ . "/../vendor/autoload.php";
-
-date_default_timezone_set("UTC");
--- dockerfile
+++ dockerfile
@@ -1,4 +1,3 @@
 FROM php:5.4-apache
-RUN docker-php-ext-install mbstring
 COPY . /var/www/
 COPY ./demo /var/www/html/
