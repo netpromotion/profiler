645f629273c9efc7798f32dd211dba94c32fec6b
Petr Knap <dev@petrknap.cz>
2016-09-27 15:12:53 +0200

    Added support for metaData

--- src/Profiler/Service/ProfilerService.php
+++ src/Profiler/Service/ProfilerService.php
@@ -14,10 +14,18 @@ class ProfilerService implements SingletonInterface
 {
     use SingletonTrait;
 
+    const META_TIME_ZERO = "meta_time_zero";
+    const META_TIME_TOTAL = "meta_time_total";
+
     const TIME_LINE_BEFORE = "time_line_before"; // int [0 - 100] percentage
     const TIME_LINE_ACTIVE = "time_line_active"; // int [0 - 100] percentage
     const TIME_LINE_INACTIVE = "time_line_inactive"; // int [0 - 100] percentage
     const TIME_LINE_AFTER = "time_line_after"; // int [0 - 100] percentage
+
+    /**
+     * @var mixed[]
+     */
+    private $metaData = [];
 
     /**
      * @var Profile[]
@@ -47,6 +55,7 @@ class ProfilerService implements SingletonInterface
      */
     public function addProfile(Profile $profile)
     {
+        $this->metaData = [];
         $this->profiles[] = $profile;
         return $profile;
     }
@@ -59,25 +68,45 @@ class ProfilerService implements SingletonInterface
         return $this->profiles;
     }
 
+    private function getMetaData()
+    {
+        if (empty($this->metaData)) {
+            if (count($this->profiles) == 0) {
+                $this->metaData[self::META_TIME_ZERO] = 0;
+                $timeEnd = 0;
+            } else {
+                $this->metaData[self::META_TIME_ZERO] = $this->profiles[0]->meta[Profiler::START_TIME];
+                $timeEnd = $this->profiles[0]->meta[Profiler::FINISH_TIME];
+                foreach ($this->profiles as $profile) {
+                    $this->metaData[self::META_TIME_ZERO] = min(
+                        $this->metaData[self::META_TIME_ZERO],
+                        $profile->meta[Profiler::START_TIME]
+                    );
+                    $timeEnd = max(
+                        $timeEnd,
+                        $profile->meta[Profiler::FINISH_TIME]
+                    );
+                }
+            }
+            $this->metaData[self::META_TIME_TOTAL] = max($timeEnd - $this->metaData[self::META_TIME_ZERO], 0.001);
+        }
+
+        return $this->metaData;
+    }
+
     public function iterateProfiles(callable $callback)
     {
-        if (count($this->profiles) == 0) {
-            $t0 = 0;
-            $tN = 0;
-        } else {
-            $t0 = $this->profiles[0]->meta[Profiler::START_TIME];
-            $tN = $this->profiles[0]->meta[Profiler::FINISH_TIME];
-            foreach ($this->profiles as $profile) {
-                $t0 = min($t0, $profile->meta[Profiler::START_TIME]);
-                $tN = max($tN, $profile->meta[Profiler::FINISH_TIME]);
-            }
-        }
-        $time = max($tN - $t0, 0.001);
-
+        $metaData = $this->getMetaData();
         foreach ($this->profiles as $profile) {
-            $profile->meta[static::TIME_LINE_BEFORE] = floor(($profile->meta[Profiler::START_TIME] - $t0) / $time * 100);
-            $profile->meta[static::TIME_LINE_ACTIVE] = floor($profile->duration / $time * 100);
-            $profile->meta[static::TIME_LINE_INACTIVE] = floor(($profile->absoluteDuration - $profile->duration) / $time * 100);
+            $profile->meta[static::TIME_LINE_BEFORE] = floor(
+                ($profile->meta[Profiler::START_TIME] - $metaData[self::META_TIME_ZERO]) / $this->metaData[self::META_TIME_TOTAL] * 100
+            );
+            $profile->meta[static::TIME_LINE_ACTIVE] = floor(
+                $profile->duration / $this->metaData[self::META_TIME_TOTAL] * 100
+            );
+            $profile->meta[static::TIME_LINE_INACTIVE] = floor(
+                ($profile->absoluteDuration - $profile->duration) / $this->metaData[self::META_TIME_TOTAL] * 100
+            );
             $profile->meta[static::TIME_LINE_AFTER] = 100 - $profile->meta[static::TIME_LINE_BEFORE] - $profile->meta[static::TIME_LINE_ACTIVE] - $profile->meta[static::TIME_LINE_INACTIVE];
 
             call_user_func($callback, $profile);
