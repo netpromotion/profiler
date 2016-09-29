4eb2d21d49e1c6ddea920ba6427a90a920f0df84
Petr Knap <dev@petrknap.cz>
2016-09-29 17:26:49 +0200

    Now uses tracy_wrap

--- demo/lumen/index.php
+++ demo/lumen/index.php
@@ -1,22 +1,34 @@
 <?php
 
 use Laravel\Lumen\Application;
+use Netpromotion\Profiler\Adapter\TracyBarAdapter;
 use Netpromotion\Profiler\Profiler;
+use Tracy\Debugger;
 
 require_once __DIR__ . "/../require_me.php";
 
 if (DEBUG_MODE === true) {
-    Profiler::enable(); // this is required only if you need to profile before container is created
+    Debugger::enable(true);
+    Profiler::enable();
 }
 
-Profiler::start(/* keep default label for better preview */);
+tracy_wrap(function() {
+    Profiler::start(/* keep default label for better preview */);
 
-$app = new Application(__DIR__);
+    $app = new Application(__DIR__);
 
-$app->get("/lumen/", function() use ($app) {
-    return $app->welcome();
-});
+    $app->get("/lumen/", function() use ($app) {
+        Profiler::start("GET /lumen/");
 
-$app->run();
+        doSomething();
+        $return = $app->welcome();
 
-Profiler::finish(/* keep default label for better preview */);
+        Profiler::finish("GET /lumen/");
+
+        return $return;
+    });
+
+    $app->run();
+
+    Profiler::finish(/* keep default label for better preview */);
+}, [new TracyBarAdapter()]);
